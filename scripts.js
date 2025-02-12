aa84e061dc8b5fdcec4a1bf301056bf18a9a1cf4
Takumi-shukutk <sshuku231116@shukutoku.ed.jp>
2025-02-12 18:07:13 +0900

    scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
-let fortune=["大吉","吉","中吉","小吉","凶"]
-let color=["red","blue","yellow","green","black"]
+let fortune=["大吉","吉","中吉","小吉","凶","大凶"]
+let color=["red","blue","yellow","green","black""bad"]
 
 let $button=document.querySelector(".button");
 let $result=document.querySelector(".result");
@@ -7,7 +7,7 @@ $button.addEventListener("click", function () {
     for(let i=0;i<5;i++){
         $result.classList.remove(color[i])
     }
-    let randomnumber=Math.floor(Math.random()*5)
+    let randomnumber=Math.floor(Math.random()*6)
     $result.innerHTML=fortune[randomnumber];
     $result.classList.add(color[randomnumber]);
 });
